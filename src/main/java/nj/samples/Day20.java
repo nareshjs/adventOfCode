3177024ab9db958dae1444af98a4eac7b8906505
Naresh Jayam <nareshjs@gmail.com>
2022-02-08 13:30:18 +0530

    Changing git user

---
+++ src/main/java/nj/samples/Day20.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+//Follow this order Day 20,21,22,24,19,23
+public class Day20 {
+  public static void main(String[] args) throws IOException {
+    List<String> lst =
+        Files.readAllLines(Paths.get("C:\\TNPS\\samples\\adventOfCode\\src\\main\\resources\\test.txt"));
+  }
+}
